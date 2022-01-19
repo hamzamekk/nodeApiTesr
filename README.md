8abf144104988f3dd52231ba3cb9f6000df7a26c
Hamza El Mekkoudi <hamza.mekkoudi@gmail.com>
2022-01-20 00:17:30 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# nodeApi
+# nodeApi
+# nodeApiTesr
---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const app =  express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const productRoutes = require('./routes/products')
+const ordersRoutes = require('./routes/orders')
+
+mongoose.connect('mongodb+srv://mekk:'+ process.env.MONGO_ATLAS_PW +'@my-node-app.yvvtf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+    if(req.method === 'OPTIONS'){
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE')
+        return res.status(200).json({})
+    }
+    next();
+})
+
+app.use('/products', productRoutes);
+app.use('/orders', ordersRoutes);
+
+app.use((req, res, next) => {
+    const error = new Error('Not Found');
+    error.status = 404;
+    next(error)
+})
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message:  error.message,
+        }
+    })
+})
+
+
+module.exports = app;

9292860f7690c5f12ab07f5439eb1d447477de6e
Hamza El Mekkoudi <hamza.mekkoudi@gmail.com>
2022-01-21 00:47:45 +0100

    add socket

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const http = require('http');
 const app = require('./app');
 
+
 const port = process.env.PORT || 3000;
 
 const server = http.createServer(app);
+
+const io = require("socket.io")(server)
+
+io.on("connection", (data) => {
+    console.log(data)
+})
 
 server.listen(port);
